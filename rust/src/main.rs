7638f0c778a7aba6bf3370a37108667f079a1c9a
qasimwarraich <qasim.warraich@gmail.com>
2022-10-25 01:28:14 +0200

    rust: Finally settled on a date-time library 😅

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,26 @@
+use chrono::{DateTime, Local};
+use std::{fs, io, time};
+
 fn main() {
-    println!("Hello, world!");
+    let path: String = "../test_dir".to_string();
+    let spam = read_files(&path);
+    println!("{}", spam.unwrap());
+}
+
+fn read_files(path: &String) -> Result<String, io::Error> {
+    for file in fs::read_dir(path)? {
+        let time = file?.metadata()?.accessed();
+
+        let datetime = format_date(time?);
+        println!("Time: {:?}", datetime.unwrap());
+    }
+    let s = String::from("Spam");
+    return Ok(s);
+}
+
+fn format_date(timestamp: time::SystemTime) -> Result<String, io::Error> {
+    let datetime: DateTime<Local> = timestamp.into();
+    let datetime = format!("{}", datetime.format("%Y-%m-%d"));
+
+    Ok(datetime)
 }
