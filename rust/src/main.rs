203ef8bce4ba93e4eb9a15b33e01878d6bd301ee
qasimwarraich <qasim.warraich@gmail.com>
2022-10-25 01:44:08 +0200

    rust: Is this more _idiomatic_ ?

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,26 @@
 use chrono::{DateTime, Local};
-use std::{fs, io, time};
+use std::{fs, io, time::SystemTime};
 
 fn main() {
-    let path: String = "../test_dir".to_string();
-    let spam = read_files(&path);
-    println!("{}", spam.unwrap());
+    let path = String::from("../test_dir");
+    read_files(&path).expect("You supplied an invalid path");
 }
 
 fn read_files(path: &String) -> Result<String, io::Error> {
+    let mut res = String::new();
+
     for file in fs::read_dir(path)? {
         let time = file?.metadata()?.accessed();
 
         let datetime = format_date(time?);
-        println!("Time: {:?}", datetime.unwrap());
+        res = datetime?;
+        println!("Time: {:?}", res);
     }
-    let s = String::from("Spam");
-    return Ok(s);
+
+    return Ok(res);
 }
 
-fn format_date(timestamp: time::SystemTime) -> Result<String, io::Error> {
+fn format_date(timestamp: SystemTime) -> Result<String, io::Error> {
     let datetime: DateTime<Local> = timestamp.into();
     let datetime = format!("{}", datetime.format("%Y-%m-%d"));
 
